5709b439ec869b0eb55de4ae62f84293038034a8
dillmandk <damiendillman@icloud.com>
2026-08-16 15:36:22 -0400

    Add BBC news sources to sources.py

---
+++ sources.py
@@ -0,0 +1,22 @@
+SOURCES = [
+    {
+        "name": "BBC World",
+        "url": "https://feeds.bbci.co.uk/news/world/rss.xml",
+        "type": "news"
+    },
+    {
+        "name": "BBC US & Canada",
+        "url": "https://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml",
+        "type": "news"
+    },
+    {
+        "name": "BBC Europe",
+        "url": "https://feeds.bbci.co.uk/news/world/europe/rss.xml",
+        "type": "news"
+    },
+    {
+        "name": "BBC Asia",
+        "url": "https://feeds.bbci.co.uk/news/world/asia/rss.xml",
+        "type": "news"
+    }
+]
